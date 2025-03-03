bd0ab97acdf9f95ac28127cdccc112f72e1eed49
Janek <janekpriv@gmail.com>
2025-03-03 19:18:10 +0100

    user_input clean version

--- user_input.c
+++ user_input.c
@@ -3,30 +3,39 @@
 
 #include "graph_generator.h"
 
-void get_input(graph *g){
+void get_input(graph *g, int nodes_count){
 
-    int nodes_count;
     int first_node;
     int second_node;
+    int edge_counter = 0;
 
-    printf("Graph generator\n----------------\nChoose graph properties: \n");
-    printf("How many nodes should graph have: ");
-    scanf("%d", &nodes_count);
-    printf("\nNow add edges by entering ids of nodes in order\n");
-    printf("f.e: 1 2 will result in 1->2 edge\n to stop adding edges type '-1'\n");
+    printf("Now add edges by entering ids of nodes in order\n");
+    printf("f.e: 1 2 will result in 1->2 edge\nto stop adding edges type '-1 -1'\n");
 
-    while(0){
-        scanf("%d %d", first_node, second_node);
+    while(1){
+
+        if(edge_counter>=(nodes_count-1)*nodes_count){
+            printf("you have reached maximum number of edges\n");
+            break;
+        }
+        
+        scanf("%d %d", &first_node, &second_node);
         if(first_node == -1){
             break;
         }   
 
-        if(!check_if_edge_exists(g)){
-            add_edge(g, first_node, second_node);
-        }else{
-            fprintf(stderr, "this edge already exists, try adding another one");
+        if (first_node < 0 || first_node >= nodes_count || second_node < 0 || second_node >= nodes_count) {
+            fprintf(stderr, "Invalid node index. Must be between 0 and %d.\n", nodes_count - 1);
             continue;
         }
+
+        if(!check_if_edge_exists(g, first_node, second_node)){
+        add_edge(g, first_node, second_node);
+        }else{
+           fprintf(stderr, "this edge already exists, try adding another one\n");
+           continue;
+        }
+        edge_counter++;
     }
 
 
