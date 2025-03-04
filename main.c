c1ce5825f08970f17411392cdb4461e1f27dfe3b
Janek <janekpriv@gmail.com>
2025-03-04 20:36:39 +0100

    prompt base generation

--- main.c
+++ main.c
@@ -4,26 +4,36 @@
 
 #include "graph_generator.h"
 #include "user_input.h"
+#include "prompt.h"
 
 int main(int argc, char **argv){
-
-
 
     int nodes_count;
     int first_node;
     int second_node;
     int edge_counter = 0;
+    int choice; 
+    char prompt[1024];
 
-    printf("Graph generator\n----------------\nChoose graph properties: \n");
-    printf("How many nodes should graph have: ");
+    printf("How would you like to enter graph properties\n0 - plain text (llm will analyze your input)\n1 - you will be asked about every property\n");
+    scanf("%d", &choice);
+
+    if(choice == 0){
+        scanf("%s", &prompt);
+        send_query(prompt);
+
+    }else{
+
+        printf("Graph generator\n----------------\nChoose graph properties: \n");
+        printf("How many nodes should graph have: ");
+        
+        scanf("%d", &nodes_count);
+        
+        graph *g  = create_graph(nodes_count);
+        
+        get_input(g, nodes_count);
     
-    scanf("%d", &nodes_count);
-    
-    graph *g  = create_graph(nodes_count);
-    
-    get_input(g, nodes_count);
-    
-    print_graph(g);
-    
+        print_graph(g);
+    }
     return 0;
 }
