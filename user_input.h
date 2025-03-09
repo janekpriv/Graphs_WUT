bcae499d328f08ac4c2bc3c894ccf6e16a162d52
Janek <janekpriv@gmail.com>
2025-03-09 16:18:20 +0100

    updated header file

--- user_input.h
+++ user_input.h
@@ -1,11 +1,11 @@
 #ifndef USER_INPUT_H
 #define USER_INPUT_H
 
-#include "graph_generator.h"
+#include "graph.h"
 
 
-void get_input(graph *g, int nodes_count);
-void generate_random_graph(graph *g);
+void get_input(Graph*g, int nodes_count);
+void generate_random_graph(Graph *g);
 
 
 
