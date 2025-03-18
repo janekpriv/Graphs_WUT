101c06c9bb4fad316bc0da1e07a053fcbbe87909
Janek <janekpriv@gmail.com>
2025-03-18 10:37:43 +0100

    structured lib files

---
+++ lib/llm.h
@@ -0,0 +1,19 @@
+#ifndef __LLM_H__
+#define __LLM_H__
+
+#include <stdlib.h>
+#include <curl/curl.h>
+#include "graph.h"
+
+Graph* parse_chat_reply(char rep[]);
+char * send_query(char * payload, CURL *curl);
+char * ask_llm(char *, char*);
+
+struct memory {
+    char *response;
+    size_t size;
+  };
+
+static size_t write_callback(char *data, size_t size, size_t nmemb, void *clientp);
+
+#endif
---
+++ lib/user_input.h
@@ -0,0 +1,13 @@
+#ifndef USER_INPUT_H
+#define USER_INPUT_H
+
+#include "graph.h"
+
+
+void get_input(Graph*g, int nodes_count);
+void generate_random_graph(Graph *g);
+
+
+
+
+#endif
